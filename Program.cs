041755f7488e9e7827d683c436a1265a8a39e480
Дмитрий Мазин <mitya.mazin@mail.ru>
2022-04-04 20:32:14 +0300

    Изменил доступность класса и добавил новую строку "Как поживаешь?"

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 
 namespace Lesson1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("How are you?");
         }
     }
 }
