f670811a3ad4c5d2c3d66e5fc21af69ab1539845
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-04-20 10:18:54 +0300

    Добавлена операция умножения чисел

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -8,10 +8,15 @@ namespace Lesson1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(Sum(2, 3, 4));
+            Console.WriteLine(Multi(2, 3));
         }
         static int Sum(int x, int y, int z)
         {
             return x + y + z;
+        }
+        static int Multi(int x, int y)
+        {
+            return x * y;
         }
     }
 }
