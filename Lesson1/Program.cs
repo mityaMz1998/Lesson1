cb43d12b74bee4dd76293cb9d8029c041619fefa
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-04-20 10:32:59 +0300

    Добавлена операция вычитания чисел

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -8,10 +8,15 @@ namespace Lesson1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(Sum(2, 3, 4));
+            Console.WriteLine(Sub(6, 1));
         }
         static int Sum(int x, int y, int z)
         {
             return x + y + z;
+        }
+        static int Sub(int x, int y)
+        {
+            return x - y;
         }
     }
 }
